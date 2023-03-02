071cb410d5cba393f352f7d26061f5da5242a3e9
Xaver <urbanxaver@hotmail.com>
2023-03-02 16:46:42 +0000

    -JUnit Tests for adding to the collection

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -18,5 +18,19 @@ class NoteAPI {
             listOfNotes
         }
     }
+    fun numberOfNotes(): Int {
+        return notes.size
+    }
+
+    fun findNote(index: Int): Note? {
+        return if (isValidListIndex(index, notes)) {
+            notes[index]
+        } else null
+    }
+
+    //utility method to determine if an index is valid in a list.
+    fun isValidListIndex(index: Int, list: List<Any>): Boolean {
+        return (index >= 0 && index < list.size)
+    }
 
 }
