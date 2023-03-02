9c37fe8b6f54db96decf8daabd800e3d0e86dbf0
Xaver <urbanxaver@hotmail.com>
2023-03-02 15:28:58 +0000

    -

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,5 @@ fun main(args: Array<String>) {
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+
 }
