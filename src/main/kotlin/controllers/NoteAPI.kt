d9c169eef01655eb1dc3e5b81d19285aa3bda97a
xaverurban <urbanxaver@hotmail.com>
2023-03-16 16:44:48 +0000

    -Add search by title

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -126,7 +126,10 @@ class NoteAPI(serializerType: Serializer) {
         }
         return false
     }
-
+fun searchByTitle (searchString : String) =
+    notes.filter { note -> note.noteTitle.contains(searchString,ignoreCase = true)  }
+        .joinToString(separator = "\n") { note->
+            notes.indexOf(note).toString() +": " +note.toString() }
 
     @Throws(Exception::class)
     fun load() {
